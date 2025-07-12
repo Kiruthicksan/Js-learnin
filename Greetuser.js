942d1a89dd01dea7755ef2b4cbbeb5b2d61b5404
Kiruthicksan <kiruthicksanboopathi@gmail.com>
2025-07-12 19:34:02 +0530

    2problems

---
+++ Greetuser.js
@@ -0,0 +1,8 @@
+let username = prompt("Enter your name")
+
+function greetUser(){
+    return `Hello, ${username}!`
+}
+
+let result = greetUser()
+console.log(result)
---
+++ areaofrectange.js
@@ -0,0 +1,7 @@
+function rectangle(l,w){
+    let area = l * w
+    return area
+}
+
+let result = rectangle(10,5)
+console.log(result)
